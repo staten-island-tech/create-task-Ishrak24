22ab158e14de4bc3ac8d7da83d6cbbc951c7d106
Ishrak24 <113196890+Ishrak24@users.noreply.github.com>
2023-03-16 22:19:34 -0400

    fixed

--- create-task/index.js
+++ create-task/index.js
@@ -1,93 +1,70 @@
-console.log("You have connected...");
+// Object to map each starter Pokemon to its type
+const pokemonTypes = {
+  Bulbasaur: ["grass", "poison"],
+  Charmander: ["fire"],
+  Squirtle: ["water"],
+  Chikorita: ["grass"],
+  Cyndaquil: ["fire"],
+  Totodile: ["water"],
+  Treecko: ["grass"],
+  Torchic: ["fire"],
+  Mudkip: ["water"],
+  Turtwig: ["grass"],
+  Chimchar: ["fire"],
+  Piplup: ["water"],
+  Snivy: ["grass"],
+  Tepig: ["fire"],
+  Oshawott: ["water"],
+  Chespin: ["grass"],
+  Fennekin: ["fire"],
+  Froakie: ["water"],
+  Rowlet: ["grass", "flying"],
+  Litten: ["fire"],
+  Popplio: ["water"],
+  Grookey: ["grass"],
+  Scorbunny: ["fire"],
+  Sobble: ["water"],
+};
 
-document.addEventListener("DOMContentLoaded", () => {
-  let generateBtn = document.querySelector("#generate-pokemon");
-  generateBtn.addEventListener("click", renderEverything);
+// Function to determine the winner of a fight
+function determineWinner(pokemon1, pokemon2) {
+  // Check if either Pokemon is missing
+  if (!pokemon1 || !pokemon2) {
+    return "Both Pokemon must be selected to fight!";
+  }
 
-  getDeleteBtn().addEventListener("click", deleteEverything);
+  // Check if the two Pokemon are the same
+  if (pokemon1 === pokemon2) {
+    return `${pokemon1} and ${pokemon2} beat each other up so much that Nurse Joy can't heal them. They die and Nurse Joy spirals into depression!`;
+  }
+
+  // Determine the types of the two Pokemon
+  const type1 = pokemonTypes[pokemon1];
+  const type2 = pokemonTypes[pokemon2];
+
+  // Determine the winner based on type advantages
+  if (type1.includes("grass") && type2.includes("water")) {
+    return `${pokemon1} gives ${pokemon2} a cut so bad ${pokemon2}'s bloodline ends!`;
+  } else if (type1.includes("water") && type2.includes("fire")) {
+    return `${pokemon1} gets ${pokemon2} so wet ${pokemon2} can't fight anymore!`;
+  } else if (type1.includes("fire") && type2.includes("grass")) {
+    return `${pokemon1} roasts ${pokemon2} so hard ${pokemon2} goes crying back to mommy!`;
+  } else if (type1.includes("water") && type2.includes("grass")) {
+    return `${pokemon2} gives ${pokemon1} a cut so bad ${pokemon1}'s bloodline ends!`;
+  } else if (type1.includes("fire") && type2.includes("water")) {
+    return `${pokemon2} gets ${pokemon1} so wet ${pokemon1} can't fight anymore!`;
+  } else if (type1.includes("grass") && type2.includes("fire")) {
+    return `${pokemon2} roasts ${pokemon1} so hard ${pokemon1} goes crying back to mommy!`;
+  } else {
+    return `${pokemon1} and ${pokemon2} are the same type, they resort to cannibalism. Professor Oak Wins!`;
+  }
+}
+
+// Event listener for the fight button
+const fightButton = document.getElementById("fight-button");
+fightButton.addEventListener("click", () => {
+  const pokemon1 = document.getElementById("pokemon1-select").value;
+  const pokemon2 = document.getElementById("pokemon2-select").value;
+  const results = document.getElementById("results");
+  results.textContent = determineWinner(pokemon1, pokemon2);
 });
-
-function renderEverything() {
-  let allPokemonContainer = document.querySelector("#poke-container");
-  allPokemonContainer.innerText = "";
-  fetchKantoPokemon();
-
-  getDeleteBtn().style.display = "block";
-}
-
-function getDeleteBtn() {
-  return document.querySelector("#delete-btn");
-}
-
-function fetchKantoPokemon() {
-  fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
-    .then((response) => response.json())
-    .then(function (allpokemon) {
-      allpokemon.results.forEach(function (pokemon) {
-        fetchPokemonData(pokemon);
-      });
-    });
-}
-
-function fetchPokemonData(pokemon) {
-  let url = pokemon.url;
-  fetch(url)
-    .then((response) => response.json())
-    .then(function (pokeData) {
-      renderPokemon(pokeData);
-    });
-}
-
-function renderPokemon(pokeData) {
-  let allPokemonContainer = document.getElementById("poke-container");
-  let pokeContainer = document.createElement("div");
-  pokeContainer.classList.add("ui", "card");
-
-  createPokeImage(pokeData.id, pokeContainer);
-
-  let pokeName = document.createElement("h4");
-  pokeName.innerText = pokeData.name;
-
-  let pokeNumber = document.createElement("p");
-  pokeNumber.innerText = `#${pokeData.id}`;
-
-  let pokeTypes = document.createElement("ul");
-
-  createTypes(pokeData.types, pokeTypes);
-
-  pokeContainer.append(pokeName, pokeNumber, pokeTypes);
-  allPokemonContainer.appendChild(pokeContainer);
-}
-
-function createTypes(types, ul) {
-  types.forEach(function (type) {
-    let typeLi = document.createElement("li");
-    typeLi.innerText = type["type"]["name"];
-    ul.append(typeLi);
-  });
-}
-
-function createPokeImage(pokeID, containerDiv) {
-  let pokeImgContainer = document.createElement("div");
-  pokeImgContainer.classList.add("image");
-
-  let pokeImage = document.createElement("img");
-  pokeImage.srcset = `https://pokeres.bastionbot.org/images/pokemon/${pokeID}.png`;
-
-  pokeImgContainer.append(pokeImage);
-  containerDiv.append(pokeImgContainer);
-}
-
-function deleteEverything(event) {
-  event.target.style = "none";
-  let allPokemonContainer = document.querySelector("#poke-container");
-  allPokemonContainer.innerText = "";
-
-  let generateBtn = document.createElement("button");
-  generateBtn.innerText = "Generate Pokemon";
-  generateBtn.id = "generate-pokemon";
-  generateBtn.classList.add("ui", "secondary", "button");
-  generateBtn.addEventListener("click", renderEverything);
-
-  allPokemonContainer.append(generateBtn);
-}
